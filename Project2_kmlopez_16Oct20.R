e8d531929d9a0c813f1b337a9567562199890cd6
klopez1 <klopez1@uncc.edu>
2020-10-16 15:30:36 -0400

    correct files created

--- Project2_kmlopez_16Oct20.R
+++ Project2_kmlopez_16Oct20.R
@@ -2,7 +2,7 @@ library(rmarkdown)
 #save unique days
 weekdays <- c("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
 #set filenames
-outFile <- paste0("../", weekdays, ".md")
+outFile <- paste0(weekdays, ".md")
 #get list for each day with the day parameter
 params = lapply(weekdays, FUN = function(x){list(weekday = x)})
 #create data frame
@@ -10,7 +10,7 @@ reports <- tibble(outFile, params)
 #use data frame to automate R Markdown reports
 apply(reports, MARGIN = 1,
       FUN = function(x){
-        render(input = "Bike-Sharing-Dataset/day.csv",
+        render(input = "Project2_kmlopez_16Oct20.Rmd",
                output_file = x[[1]],
                params = x[[2]])
       })
